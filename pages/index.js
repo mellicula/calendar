7f4f19b7e81619895106dd7f21ed1317abf1cc1b
Honey Raut <hraut.au@gmail.com>
2025-05-05 07:25:50 +1000

    supabase auth

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,41 @@
+import { supabase } from '../lib/supabaseClient';
+import { useState } from 'react';
+
 export default function Home() {
-	return (
-		<div style={{ padding: '2rem', fontFamily: 'sand-serif'}}>
-			<h1> Welcome!</h1>
-			<button>Log in</button> &nbsp;
-			<button>Sign Up</button>
-			<br /><br />
-			<input type="text" placeholder="typesomething"/>
-		</div>
-	);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signUp = async () => {
+    const { error } = await supabase.auth.signUp({ email, password });
+    if (error) alert(error.message);
+    else alert('check ur email for confirmation link');
+  };
+  const signIn = async () => {
+    const { error } = await supabase.auth.signInWithPassword({ email, password });
+    if (error) alert(error.message)
+      else alert('logged in');
+  };
+
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Welcome!</h1>
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      /><br /><br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      /><br /><br />
+
+      <button onClick={signIn}>Log In</button> &nbsp;
+      <button onClick={signUp}>Sign Up</button>
+    </div>
+  );
 }
