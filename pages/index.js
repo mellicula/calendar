d16296e0094d104b3597a2f06231bfb86edd0f5b
Honey Raut <hraut.au@gmail.com>
2025-05-05 07:05:20 +1000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+	return (
+		<div style={{ padding: '2rem', fontFamily: 'sand-serif'}}>
+			<h1> Welcome!</h1>
+			<button>Log in</button> &nbsp;
+			<button>Sign Up</button>
+			<br /><br />
+			<input type="text" placeholder="typesomething"/>
+		</div>
+	);
+}
